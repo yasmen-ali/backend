219ccf431994c403fbf11a1bff9b1fd31b9f4d1c
yasmen-ali <ya7381188@gmail.com>
2025-05-03 21:26:54 +0300

    task1

---
+++ backend.php
@@ -0,0 +1,62 @@
+<?php
+
+	$firstName = " yasmen ";
+	$medelName = ' ali ';
+	$lastName = ' ismail ';
+	$finalName = ' ali ';
+
+
+	echo $firstName . '' . $medelName . '' . $lastName . '' . $finalName ;
+
+	echo " <br> " ;
+
+	print $firstName . '' . $medelName . '' . $lastName . '' . $finalName ;
+	
+
+echo" <br> ";
+
+	const universte = ' elminia ' ;
+	 
+	echo universte  ;
+
+	echo "<br>";
+
+	$cors="hello from black horse";
+	echo $cors;
+    echo "<br>" ;
+	
+	echo strtoupper($cors);
+	echo "<br>";
+	$num=" Osama Is A Programmer";
+	echo $num ;
+	echo"<br>";
+	$num="Osama Is A Programmer";
+	echo strtolower(string: $num );
+	echo "<br>";
+	echo $num ;
+	 echo "<br>";
+     $name=" osama is programmer";
+	 echo $name ;
+	 echo " <br> " ;
+	 echo strrev("hello from black hourse");
+	 echo "<br>";
+	 $post="#########hello############";
+	 echo $post ;
+	 echo"<br>";
+	 echo trim($post, "#") ;
+	 echo "<br>";
+	 
+	 
+
+
+	
+
+
+
+
+
+
+
+
+
+?>
