12021258b91ea5389df8b12eaa16dbea44b673a0
yasmen-ali <ya7381188@gmail.com>
2025-05-05 22:01:58 +0300

    task2

---
+++ task 1 back end .php
@@ -0,0 +1,115 @@
+<?php
+
+	$firstName = " yasmen ";
+	$medelName = ' ali ';
+	$lastName = ' ismail ';
+	$age = '37';
+
+echo $firstName ;
+echo "<br>";
+echo $medelName ;
+echo "<br>";
+echo $lastName ;
+echo "<br>";
+echo $age ;
+echo "<br>";
+
+
+
+echo  $firstName . '' .  '' . $lastName . ''   ;
+
+echo " <br> " ;
+
+print $firstName . '' .  '' . $lastName . '' ;
+	
+
+
+echo" <br> ";
+# 5  variables print in a different lines.
+
+
+	const universte = ' elminia ' ;
+	 
+	echo universte  ;
+	echo "<br>";
+
+	$cors="hello from black horse";
+	echo $cors;
+    echo "<br>" ;
+	echo strtoupper($cors);
+	echo "<br>";
+	echo"<br>";
+	$neam=" Osama Is A Programmer";
+	echo $neam ;
+	echo strtolower($name);
+	echo "<br>";
+	echo $neam ;
+	 echo "<br>";
+	 $name=substr(	$name ( $name ),offset: 6 ,length:10 );
+    echo $name ;
+	 echo " <br> " ;
+	 echo strrev("hello from black hourse");
+	 echo "<br>";
+
+     # TRIM -----
+
+	 $post=" #########hello############";
+	 echo $post ;
+	 echo"<br>";
+	 echo trim($post, "#") ;
+	 echo "<br>";
+	 $first="yasmen";
+	 echo "<br>";
+	 $yasmen;
+	 echo "<br>";
+
+	 // two constant using two different methods.
+
+
+	 define ( 'CAR', "toyota"); 
+	 echo CAR;
+	 echo "<br>";
+	 const UNIT = "one";
+	 echo UNIT;
+	 echo "<br>";
+	 
+	/* give an array of names prit all names
+	 that have 5 character length - print not echo */
+
+
+	 $neams= [ "osama" , 'ahmed', 'nada', 'omar' , 'mai'];
+	 echo "<br>";
+	 echo $noor;
+	 	 
+	
+
+
+
+	 
+
+
+	
+
+
+
+
+
+
+
+
+
+?>
+
+
+   
+
+
+
+	
+
+
+
+
+
+
+
